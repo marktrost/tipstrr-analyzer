b3f639c0ecc15f03b56b784b29fd224d0b90dc62
marktrost <kzgansta@gmail.com>
2026-01-16 20:54:51 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Используем переменную окружения от Render
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
